1f39802fba86a2574486a7e1b974233f7b525469
Dan H <spag1024@hotmail.com>
2019-05-28 05:38:00 -0500

    working with debug settings

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('combined'));
+
 app.get('/', function(req, res){
     res.send('test from express.get');
 });
 
 app.listen(3000, function(){
-    debug('listening on port 3000. ctrl+c to stop');
+    debug(chalk.green('listening on port 3000. ctrl+c to stop'));
 });
