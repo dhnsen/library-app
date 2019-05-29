e2cd6fc284894fb3d8ccf6708d65e813eac3a555
Dan H <spag1024@hotmail.com>
2019-05-29 06:06:56 -0500

    switch bootstrap and jquery from cdn to npm

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const chalk = require('chalk');
-const debug = require('debug');
+const debug = require('debug')('app');
 const morgan = require('morgan');
-
+const path = require('path');
 
 const app = express();
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '\\views\\index.html');
+    res.sendfile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.listen(3000, function () {
