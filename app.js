5de9de4b27d7d755b036098291864592a7eb37f2
Dan H <spag1024@hotmail.com>
2019-05-31 06:19:21 -0500

    did some testing in pug

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('views', path.join('public', 'src', 'views'));
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
-    res.render('index');
+    res.render('index', { title: 'MyLibrary' });
 });
 
 app.listen(3000, function () {
