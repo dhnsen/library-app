e2c1e3fe343478c6d5e125d84009cf6253928563
Dan H <spag1024@hotmail.com>
2019-05-31 06:08:49 -0500

    installed put and set express to use it

--- app.js
+++ app.js
@@ -7,11 +7,16 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+
+app.set('views', path.join('.', 'src', 'views'));
+app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
     res.sendfile(path.join(__dirname, 'views', 'index.html'));
