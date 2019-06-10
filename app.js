859f72be9e466c701ed8727722ccda8b0bed97e0
Dan H <spag1024@hotmail.com>
2019-06-10 07:31:13 -0500

    created a SQL connection and tested. Connection was successful.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const sql = require('mssql');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,10 +11,23 @@ const port = process.env.PORT || 3000;
 // Going to add MS SQL Database access here and use "adminRoutes"
 // need to figure out connection string for express
 // here is asp.net connection string:
-// "Data Source=SQL5018.site4now.net;Initial Catalog=DB_A3D314_library;User Id=DB_A3D314_library_admin;Password=LIBRARY2019;"
+// "Data Source=SQL5018.site4now.net;Initial Catalog=DB_A3D314_library;
+// User Id=DB_A3D314_library_admin;Password=LIBRARY2019;"
 
 // by the way, make a note that asp.net hosting expires in 6 months!...
 
+const config = {
+    user: 'DB_A3D314_library_admin',
+    password: 'LIBRARY2019',
+    server: 'SQL5018.site4now.net', // You can use 'localhost\\instance' to connect to named instance
+    database: 'DB_A3D314_library',
+ 
+    options: {
+        encrypt: true // Use this if you're on Windows Azure
+    }
+};
+
+sql.connect(config).catch((err) => debug(err));
 
 const nav = [{ link: '/books', title: 'Book' },
 { link: '/authors', title: 'Author' }]
