528ce10c8a14049686f9247c9f52418a4378fc60
Dan H <spag1024@hotmail.com>
2019-06-19 05:45:31 -0500

    Revert "working on single book route. created route query and fixed a bug with the recordset"
    This reverts commit 9274381cfe4192182f6dc72e322c6727b1db5d3c.

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -38,22 +38,14 @@ function router(nav) {
 
 
     bookRouter.route('/:id').get(function (req, res) {
-        (async function query() {
-            const { id } = req.params;
-            const request = new sql.Request();
-            const result = await request
-            .input('id', sql.Int, id)
-            .query('select * from books where id = @id');
-            debug(result);
-            res.render('bookView',
-                {
-                    title: 'Library',
-                    nav,
-                    book: result.recordset[0]
-                }
-            );
-        }());
-
+        const { id } = req.params;
+        res.render('bookView',
+            {
+                title: 'Library',
+                nav,
+                book: books[id]
+            }
+        );
     });
     return bookRouter;
 }
