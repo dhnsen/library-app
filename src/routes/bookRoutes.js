8cb835e422a4f725cdddd64a2feb048a5ff40c28
Dan H <spag1024@hotmail.com>
2019-06-24 06:19:51 -0500

    started setting up DB call for single

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bookRouter = express.Router();
 const debug = require('debug')('app:bookRoutes');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 
 function router(nav) {
 
@@ -39,6 +39,23 @@ function router(nav) {
   bookRouter.route('/:id')
     .get((req, res) => {
       const { id } = req.params;
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('connected to DB server on books');
+
+          const db = client.db(dbName);
+          const col = await db.collection('books');
+
+          const book = col.findOne({ _id: new ObjectID(id) });
+        } catch (err) {
+          debug(err.stack);
+        }
+      }());
       res.render(
         'bookView',
         {
