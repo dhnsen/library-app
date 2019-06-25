e81e90e1ba82f5c87cc1319491b8f2ec07c4aeb0
Dan H <spag1024@hotmail.com>
2019-06-25 06:21:16 -0500

    set up bodyParser and worked on authRoutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,3 +1,5 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
 const express = require('express');
 const debug = require('debug')('app:bookRoutes');
 const { MongoClient, ObjectID } = require('mongodb');
@@ -7,8 +9,8 @@ const authRouter = express.Router();
 function router() {
     authRouter.route('/signUp')
         .post((req, res) => {
-
+            debug(req.body);
         });
-};
+}
 
-module.exports router;
+module.exports = router;
