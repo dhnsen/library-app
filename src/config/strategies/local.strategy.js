033cafae9352060bf25dadaf077fe64aa7f1a0e8
Dan H <spag1024@hotmail.com>
2019-06-29 05:48:44 -0500

    finished authorization in local strategy

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -1,6 +1,8 @@
 /* eslint-disable linebreak-style */
 const passport = require('passport');
 const { Strategy } = require('passport-local');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:local.strategy');
 
 module.exports = function localStrategy() {
   passport.use(new Strategy(
@@ -8,10 +10,31 @@ module.exports = function localStrategy() {
       usernameField: 'username',
       passwordField: 'password',
     }, (username, password, done) => {
-      const user = {
-        username, password,
-      };
-      done(null, user);
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+
+          debug('connected to DB server on local.strategy');
+
+          const db = client.db(dbName);
+          const col = db.collection('users');
+
+          const user = await col.findOne({ username });
+
+          if (user.password === password) {
+            done(null, user);
+          } else {
+            done(null, false);
+          }
+        } catch (err) {
+          debug(err);
+        }
+        client.close();
+      }());
     },
   ));
 };
